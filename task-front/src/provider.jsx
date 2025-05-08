7ad9a03d8f2284203698847e6edb9c4a3c17a852
Jose Leonardo Martinez Gomez <20223tn067@utez.edu.mx>
2025-05-08 08:52:15 -0600

    crear proyecto

---
+++ task-front/src/provider.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { HeroUIProvider } from "@heroui/system";
+import { useHref, useNavigate } from "react-router-dom";
+
+export function Provider({ children }) {
+  const navigate = useNavigate();
+
+  return (
+    <HeroUIProvider navigate={navigate} useHref={useHref}>
+      {children}
+    </HeroUIProvider>
+  );
+}
